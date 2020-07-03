9a0ddad2b90d6c55199664d4bfb8d93d8d2259a5
Gustavo Hernandez <gustavoh@onetree.com>
2020-07-02 23:42:44 -0300

    Setting up youtube service

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class YoutubeService {
 
-  constructor() { }
+  // https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=&playlistId=&maxResults=10
+  private youtubeUrl = 'https://www.googleapis.com/youtube/v3';
+  private apiKey = 'YOUR-API-KEY';
+  private playlistId = 'UUuaPTYj15JSkETGnEseaFFg';
+  private nextPageToken = '';
+
+  constructor( private http: HttpClient ) { }
+
+  getVideos() {
+    const url = `${this.youtubeUrl}/playlistItems`;
+    const params = new HttpParams()
+      .set('part', 'snippet')
+      .set('maxResults', '10')
+      .set('playlistId', this.playlistId)
+      .set('key', this.apiKey);
+    return this.http.get(url, { params });
+  }
 }
