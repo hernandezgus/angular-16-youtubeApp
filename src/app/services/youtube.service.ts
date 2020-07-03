e6edc4c833e1e03291db7c159e0dfac3e48588bf
Gustavo Hernandez <gustavoh@onetree.com>
2020-07-02 23:56:51 -0300

    Applying pipe to youtube response

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { YoutubeResponse } from '../models/youtube.models';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -21,6 +23,13 @@ export class YoutubeService {
       .set('maxResults', '10')
       .set('playlistId', this.playlistId)
       .set('key', this.apiKey);
-    return this.http.get(url, { params });
+    return this.http.get<YoutubeResponse>(url, { params })
+      .pipe(
+        map( resp => {
+          this.nextPageToken = resp.nextPageToken;
+          return resp.items;
+        }),
+        map( items => items.map(item => item.snippet) )
+      );
   }
 }
